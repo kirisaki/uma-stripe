a7a5ac51318a6f7dc30a6e995f6494114816232b
Akihito Kirisaki <kirisaki@klara.works>
2023-06-11 20:50:07 +0900

    feat: add drawer

--- src/components/Panel.tsx
+++ src/components/Panel.tsx
@@ -1,6 +1,17 @@
+import {useState} from 'react'
+import styles from './Panel.module.css'
+
 function Panel() {
+  const [visible, setVisible] = useState(true)
   return (
-    <div>
+    <div className={`${styles.panel} ${visible ? '' : styles.close}`}>
+      <button onClick={() => visible ? setVisible(false) : setVisible(true)} className={styles.toggle}>
+        {visible
+          ? 'close'
+          : 'open'
+        }
+      </button>
+      <h1>uma-stripe</h1>
       nyaan
     </div>
   )
