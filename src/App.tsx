ac0f03d197a17ed3a0108bf89dfb9921a41d96d6
Akihito Kirisaki <kirisaki@klara.works>
2023-06-11 23:56:14 +0900

    feat: wrap by container

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import './App.css'
+import styles from './App.module.css'
 import Panel from './components/Panel'
 import Stripe from './components/Stripe'
 
 function App() {
   return (
-    <>
+    <div className={styles.container}>
       <Panel />
       <Stripe />
-    </>
+    </div>
   )
 }
 
